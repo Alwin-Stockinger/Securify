7116a86c04bf72a989e3594684863e550580b4dc
Lujza <lanyi@protonmail.com>
2018-04-27 13:40:08 +0200

    played around with tablayouts

--- app/src/main/java/com/securify/securify/MainActivity.java
+++ app/src/main/java/com/securify/securify/MainActivity.java
@@ -1,13 +1,138 @@
 package com.securify.securify;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), MainActivity.this);
+
+        pagerAdapter.addPage(new GameFragment());
+        pagerAdapter.addPage(new OtherFragment());
+        vp.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(vp);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setCustomView(pagerAdapter.getTabView(i));
+        }
     }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> pages = new ArrayList<>();
+
+        String[] tabTitles =  new String[] {"Spiele", "Anderes"};
+        Context context;
+
+        public PagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    return new GameFragment();
+                case 1:
+                    return new OtherFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return pages.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        public View getTabView(int position){
+            View tab = LayoutInflater.from(context).inflate(R.layout.custom_tab, null);
+            TextView tv = tab.findViewById(R.id.custom_text);
+            tv.setText(tabTitles[position]);
+            return tab;
+        }
+
+        //needed?
+        public void addPage(Fragment f) {
+            pages.add(f);
+        }
+    }
+
+    /*
+    private void addPages(ViewPager viewPager){
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), MainActivity.this);
+        pagerAdapter.addPage(new GameFragment());
+        pagerAdapter.addPage(new OtherFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    private TabLayout.OnTabSelectedListener tabSelectedListener(final ViewPager pager){
+        return new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        };
+    }*/
 }
