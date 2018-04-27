88f03b60ec8a670109235499bdd8c8a1314f20cb
Lujza <lanyi@protonmail.com>
2018-04-27 13:40:08 +0200

    played around with tablayouts

---
+++ app/src/main/java/com/securify/securify/GameFragment.java
@@ -0,0 +1,23 @@
+package com.securify.securify;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.game_fragment, null);
+        return rootView;
+    }
+
+    @Override
+    public String toString() {
+        return "GameTest";
+    }
+}
