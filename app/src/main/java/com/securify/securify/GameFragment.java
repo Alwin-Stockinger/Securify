aefa32f20d068778849bea38de99ab31643976f0
Lujza <lanyi@protonmail.com>
2018-05-01 10:27:52 +0200

    made a game layout template

--- app/src/main/java/com/securify/securify/GameFragment.java
+++ app/src/main/java/com/securify/securify/GameFragment.java
@@ -1,6 +1,7 @@
 package com.securify.securify;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,26 +12,58 @@ import android.widget.ProgressBar;
 
 public class GameFragment extends Fragment {
 
+    private int pStatus = 0;
+    private Handler handler = new Handler();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.game_fragment, null);
+        View rootView = inflater.inflate(R.layout.home, null);
         return rootView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ImageButton phishingButton = view.findViewById(R.id.phishing_game_text);
-        ImageButton passwordButton = view.findViewById(R.id.password_game_text);
-        ImageButton permissionsButton = view.findViewById(R.id.permissions_game_text);
-        ProgressBar phishingBar = view.findViewById(R.id.phishingProgressBar);
-        ProgressBar passwordBar = view.findViewById(R.id.passwordProgressBar);
-        ProgressBar permissionsBar = view.findViewById(R.id.permissionsProgressBar);
+        ImageButton phishingButton = view.findViewById(R.id.phishingButton);
+        ImageButton passwordButton = view.findViewById(R.id.passwordButton);
+        ImageButton permissionsButton = view.findViewById(R.id.permissionsButton);
+        final ProgressBar phishingBar = view.findViewById(R.id.phishingProgressBar);
+        final ProgressBar passwordBar = view.findViewById(R.id.passwordProgressBar);
+        final ProgressBar permissionsBar = view.findViewById(R.id.permissionsProgressBar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (pStatus <= 100) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            phishingBar.setProgress(pStatus);
+                            passwordBar.setProgress(pStatus);
+                            permissionsBar.setProgress(pStatus);
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    pStatus++;
+                }
+            }
+        }).start();
+
     }
+
+    public void onClick(View v){
+
+    }
+
 
     @Override
     public String toString() {
         return "GameTest";
     }
+
 }
