77786d3338e9c94efe43a0100a3d2752e10a3878
Lujza <lanyi@protonmail.com>
2018-05-01 11:17:15 +0200

    merge with master

--- app/src/main/java/com/securify/securify/MainActivity.java
+++ app/src/main/java/com/securify/securify/MainActivity.java
@@ -8,13 +8,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
