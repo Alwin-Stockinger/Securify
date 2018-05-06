f9d75019106f72c89269b5c19d6bd9cacfe00414
Alwin <stockingeralwin@gmail.com>
2018-05-06 18:53:53 +0200

    Code Clean-Up

--- app/src/main/java/com/securify/securify/model/MainModel.java
+++ app/src/main/java/com/securify/securify/model/MainModel.java
@@ -66,67 +66,6 @@ public class MainModel {
         return dao.getMax();
     }
 
-    //testinset
-    private void pInsert(){
-        PasswordDao dao=db.passwordDao();
-        PasswordModel model=new PasswordModel();
-        dao.deleteGame(1);
-
-        model.setId(1);
-        model.setMax_length(15);
-        model.setMin_length(5);
-        model.setMin_number(1);
-        model.setMin_upper(1);
-
-
-        dao.insertGame(model);
-    }
-
-
-
-    private void permInsert(){
-        PermissionDao dao=db.permissionDao();
-        PermissionModel model=new PermissionModel();
-        dao.deleteGame(1);      //Delete old Game with ID 1 from DB
-
-        //set all test values
-        model.setId(1);
-        model.setKamera(true);
-        model.setKontake(false);
-        model.setMikrofon(false);
-        model.setPosition(true);
-        model.setSensoren(false);
-        model.setSpeicher(true);
-        model.setErklaerung("Das ist eine Erklaerung die dir sagt was du falsch gemach hast");
-        model.setZeit(50);  //50 sekunden Zeit
-        model.setSprache("DE"); //Sprache ist Deutsch
-        model.setKontext("Du Willst eine Foto App isntallieren die zusätzlich noch den Ort deiner Fotos speichert");
-        model.setSchwierigkeit("default");
-        model.setTipp("Um den Ort zu speichern muss die App wissen wo du dich gerade befindest");
-
-        dao.insertGame(model);  //insert into DB
-    }
-
-    private void phishInsert(){
-        PhishingDao dao=db.phishingDao();
-        PhishingModel model=new PhishingModel();
-        dao.deleteGame(1);      //Delete old Game with ID 1 from DB
-
-        //set all test values
-        model.setId(1);
-        model.setAbsender("Ich bin der Absender");
-        model.setBetreff("Ich bin der Betreff");
-        model.setIs_phishing(true);
-        model.setErklaerung("Das ist eine Erklaerung die dir sagt was du falsch gemach hast");
-        model.setZeit(50);  //50 sekunden Zeit
-        model.setSprache("DE"); //Sprache ist Deutsch
-        model.setKontext("Ich bin ein gemeiner schurke und möchte deine Kreditkarte stehlen, also schick mir bitte deine Kreditkartennummer und deine PIN");
-        model.setSchwierigkeit("default");
-        model.setTipp("Schurken sollten man nicht trauen!");
-
-        dao.insertGame(model);  //insert into DB
-    }
-
 
 
 
