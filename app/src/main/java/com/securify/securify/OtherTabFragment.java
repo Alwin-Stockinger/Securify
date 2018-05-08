e6e1dfd2cf8ba4c7d108730d397b0551f8dd823b
Lujza <lanyi@protonmail.com>
2018-05-08 22:57:55 +0200

    created corresponding activity classes to layouts

--- app/src/main/java/com/securify/securify/OtherTabFragment.java
+++ app/src/main/java/com/securify/securify/OtherTabFragment.java
@@ -1,13 +1,20 @@
 package com.securify.securify;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class OtherTabFragment extends Fragment {
+
+    Button settingsButton;
+    Button highscoresButton;
+    Button awardsButton;
 
     @Nullable
     @Override
@@ -19,5 +26,49 @@ public class OtherTabFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        settingsButton = view.findViewById(R.id.settingsButton);
+        highscoresButton = view.findViewById(R.id.highscoreButton);
+        awardsButton = view.findViewById(R.id.awardsButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OtherTabFragment.this.onClick(v,"settings");
+            }
+        });
+
+        highscoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OtherTabFragment.this.onClick(v,"highscores");
+            }
+        });
+
+        awardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OtherTabFragment.this.onClick(v,"awards");
+            }
+        });
+    }
+
+    private void onClick(View v, String button_name) {
+        Intent i = new Intent();
+        switch(button_name){
+            case "settings":
+                i = new Intent(getActivity(), SettingsActivity.class);
+                break;
+            case "highscores":
+                i = new Intent(getActivity(), HighscoresActivity.class);
+                break;
+            case "awards":
+                i = new Intent(getActivity(), AwardsActivity.class);
+                break;
+
+                default:
+                    Log.wtf("onClick", "Something went terribly wrong with a button.");
+        }
+        startActivity(i);
+        Log.d("intent","Cannot start activity.");
     }
 }
