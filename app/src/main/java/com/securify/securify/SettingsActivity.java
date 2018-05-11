7788db32caf947e4324019222ed545dab15172a4
Alwin <stockingeralwin@gmail.com>
2018-05-11 22:12:53 +0200

    Settings with User change

--- app/src/main/java/com/securify/securify/SettingsActivity.java
+++ app/src/main/java/com/securify/securify/SettingsActivity.java
@@ -3,12 +3,41 @@ package com.securify.securify;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SettingsActivity extends AppCompatActivity{
+import com.securify.securify.model.MainModel;
+
+public class SettingsActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    EditText userNameInput;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        MainModel model=new MainModel(getApplicationContext());
+
+        //Assign layout items
+        userNameInput=findViewById(R.id.user_name_inputfield);
+        userNameInput.setText(model.getActiveUser().getName());
+        Button userNameButton=findViewById(R.id.user_name_confirm_btn);
+        userNameButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        MainModel model=new MainModel(getApplicationContext());
+        model.changeUser(userNameInput.getText().toString());
+
+
     }
 }
