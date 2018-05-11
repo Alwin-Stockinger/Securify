29fdbad5bf9b3d382099fd55b7617c03c3809269
Lujza <lanyi@protonmail.com>
2018-05-11 21:19:45 +0200

    added change username and change language buttons to settings

--- app/src/main/java/com/securify/securify/SettingsActivity.java
+++ app/src/main/java/com/securify/securify/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.securify.securify;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SettingsActivity extends AppCompatActivity{
 
@@ -10,5 +12,9 @@ public class SettingsActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        ImageButton backButton = findViewById(R.id.backButton);
+        Button changeLang = findViewById(R.id.changeLanguage);
+        Button changeUname = findViewById(R.id.changeUsername);
     }
 }
